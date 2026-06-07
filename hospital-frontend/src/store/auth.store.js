26b13833df65e37fe0c3e5fe2283835cb3b66cb1
TRS-VISHAL <vishalsambare2004@gmail.com>
2026-06-07 17:22:06 +0530

    compatible with cookies and headers

--- hospital-frontend/src/store/auth.store.js
+++ hospital-frontend/src/store/auth.store.js
@@ -34,6 +34,7 @@ const useAuthStore = create((set) => ({
     try {
       await logout();
       localStorage.removeItem('accessToken');
+      navigate('/login');
     } catch {
       // ignore
     }
