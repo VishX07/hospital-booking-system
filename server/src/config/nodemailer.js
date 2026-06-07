a3914188d01b8bf9c429b3a8e92ef2415f458091
TRS-VISHAL <vishalsambare2004@gmail.com>
2026-06-07 20:42:02 +0530

    new smtp model added brevo

--- server/src/config/nodemailer.js
+++ server/src/config/nodemailer.js
@@ -25,18 +25,45 @@
 
 // export default transporter;
 
-import { Resend } from 'resend';
+// import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
+
+// export const sendEmail = async ({ to, subject, html }) => {
+//   const { data, error } = await resend.emails.send({
+//     from: '<onboarding@resend.dev>', // use this until you add your domain
+//     to,
+//     subject,
+//     html,
+//   });
+
+//   if (error) throw new Error(error.message);
+//   return data;
+// };
 
 export const sendEmail = async ({ to, subject, html }) => {
-  const { data, error } = await resend.emails.send({
-    from: 'AlphaCare <onboarding@resend.dev>', // use this until you add your domain
-    to,
-    subject,
-    html,
+  const response = await fetch('https://api.brevo.com/v3/smtp/email', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'api-key': process.env.BREVO_API_KEY,
+    },
+    body: JSON.stringify({
+      sender: {
+        name: 'Alpha Hospital',
+        email: 'alphahospital.app@gmail.com', // verified sender
+      },
+      to: [{ email: to }],
+      subject,
+      htmlContent: html,
+    }),
   });
 
-  if (error) throw new Error(error.message);
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(JSON.stringify(data));
+  }
+
   return data;
 };
